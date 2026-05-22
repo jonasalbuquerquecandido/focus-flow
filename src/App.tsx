0eac7dc9406a411b3cf70bcc10649c198779d818
jonasalbuquerquecandido <jonasalbuquerque.c@gmail.com>
2026-05-21 21:23:13 -0300

    I added new colors in the theme.css and i organized the Heading function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './styles/theme.css'
 import './styles/global.css'
-import { Heading } from './components/Heading/Heading.tsx'
+import { Heading } from './components/Heading';
+
+
 
 
 export function App() {
     return (
         <>
-            <Heading />
+
+        <Heading>
+            Olá, Mundo!
+        </Heading>
+
         </>
-    )
+    );
 }
