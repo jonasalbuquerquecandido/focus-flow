4d90f1220b729f844bfb329515cc11f3d1980077
jonasalbuquerquecandido <jonasalbuquerque.c@gmail.com>
2026-05-22 20:30:15 -0300

    I've added new settings for global.css and organized the structure of the sidebar HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import './styles/theme.css'
-import './styles/global.css'
+import './styles/theme.css';
+import './styles/global.css';
 import { Container } from './components/Container';
 import { Sidebar } from './components/Sidebar';
 
 export function App() {
-    return (
-        <>
-        <Container>
-            <Sidebar />
-        </Container>
-        </>
-    );
+  return (
+    <>
+      <Container>
+        <Sidebar />
+      </Container>
+    </>
+  );
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,34 +1,49 @@
-import style from './style.module.css'
+import style from './style.module.css';
 
 export function Sidebar() {
-    return (
-        <div className={style.sidebar}>
-            <div className={style.logo}>
-                <img src="./assets/logo.png" alt="Logo" />
-                <h2>Focus Flow</h2>
-            </div>
-            <ul>
-                    <li className={style.itens}>
-                        <i className="fas fa-home"></i>
-                        <a href="#">Início</a>
-                    </li>
-                    <li className={style.itens}>
-                        <i className="fas fa-history"></i>
-                        <a href="#">Histórico</a>
-                    </li>
-                    <li className={style.itens}>
-                        <i className="fas fa-tasks"></i>
-                        <a href="#">Hábitos</a>
-                    </li>
-                    <li className={style.itens}>
-                        <i className="fas fa-chart-line"></i>
-                        <a href="#">Dashboard</a>
-                    </li>
-                    <li className={style.itens}>
-                        <i className="fas fa-cog"></i>
-                        <a href="#">Configurações</a>
-                    </li>
-            </ul>
+  return (
+    <div className={style.sidebar}>
+      <div className={style.logo}>
+        <img src='./assets/logo.png' alt='Logo' />
+        <h1>Focus Flow</h1>
+      </div>
+
+      <ul className={style.menu}>
+        <li className={style.itens}>
+          <i className='fas fa-home'></i>
+          <a href='#'>Início</a>
+        </li>
+        <li className={style.itens}>
+          <i className='fas fa-history'></i>
+          <a href='#'>Histórico</a>
+        </li>
+        <li className={style.itens}>
+          <i className='fas fa-tasks'></i>
+          <a href='#'>Hábitos</a>
+        </li>
+        <li className={style.itens}>
+          <i className='fas fa-chart-line'></i>
+          <a href='#'>Dashboard</a>
+        </li>
+        <li className={style.itens}>
+          <i className='fas fa-cog'></i>
+          <a href='#'>Configurações</a>
+        </li>
+      </ul>
+
+      <div>
+        <i className='fas fa-moon'></i>
+        Tema escuro
+        <button>Ativar</button>
+      </div>
+
+      <div>
+        <img src='./assets/profile.png' alt='Profile' />
+        <div>
+          Lucas Andrade
+          <p>Status</p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
