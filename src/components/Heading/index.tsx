02191c1c03088b026b184d41ca52ab7b6633f58b
jonasalbuquerquecandido <jonasalbuquerque.c@gmail.com>
2026-05-28 22:44:21 -0300

    i added and styled Heading component

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -4,11 +4,11 @@ const name = 'Gustavo';
 export function Heading() {
   return (
    <header className={style.header}>
-    <div>
-        <h2>Seja bem-vindo {name}!</h2>
+    <div className={style.heading}>
+        <h1>Seja bem-vindo <span>{name}</span>!</h1>
         <p>Vamos manter o foco e alcançar nossos objetivos!</p>
     </div>
-    <div>
+    <div className={style.icons}>
         <i className="fa-regular fa-bell"></i>
         <i className="fa-solid fa-chart-line"></i>
     </div>
