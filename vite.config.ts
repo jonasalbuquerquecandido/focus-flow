d88b7805304424e1f68e778c83021af1f876967a
jonasalbuquerquecandido <jonasalbuquerquecandido@gmail.com>
2026-06-22 23:19:22 -0300

    deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
-  base:'/focus-flow/',
+  base: '/focus-flow/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
